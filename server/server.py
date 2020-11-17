fd5e255b57778f221e3bc31e7197dbf8b98db0a2
Ubuntu <ubuntu@ip-172-31-38-159.eu-west-2.compute.internal>
2020-11-17 23:13:31 +0000

    it works through api gateway

--- server/server.py
+++ server/server.py
@@ -27,7 +27,10 @@ def root():
 
     return json.dumps({
         'statusCode': 200,
-        'response': img
+        'response': img,
+        'headers': {
+            'Access-Control-Allow-Origin': '*'
+        }
     })
 
 @app.route('/predict', methods=['POST'])
@@ -41,10 +44,10 @@ def predict():
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,access-control-allow-origin')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')#,access-control-allow-origin')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
   return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=5000)
